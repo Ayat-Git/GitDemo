6133e551816f4d8d3b96f504161d5a8706472283
Ayat-Git <ayatconvtch@gmail.com>
2019-10-24 22:17:14 +0300

    My Second commit

--- GitDemo/src/com/atypon/Main.java
+++ GitDemo/src/com/atypon/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("My Demo in Git");
+        System.out.println("New change  ");
     }
 }
